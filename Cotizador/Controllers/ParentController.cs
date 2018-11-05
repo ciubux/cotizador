e13df74eed3223441b555018a3c1c9384af5b985
Yrving Ramirez <yrvingrl520@gmail.com>
2018-11-04 20:28:38 -0500

    Email alerta pedidos sin atencion, redireccionar a url solicitada luego de login

---
+++ Cotizador/Controllers/ParentController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+using System.Web.Routing;
+
+
+namespace Cotizador.Controllers
+{
+    public class ParentController : Controller
+    {
+        public Usuario Logueado;
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            var isLogin = true;
+
+            if (this.Session[Constantes.VAR_SESSION_USUARIO] == null)
+            {
+                string fullUrl = Request.Url.AbsoluteUri;
+                this.Session["Prev_Request_Url"] = fullUrl;
+
+                RedirectToAction("Login", "Account");
+            }
+            else
+            {
+                
+                Logueado = (Usuario)this.Session[Constantes.VAR_SESSION_USUARIO];
+                //Obtener controller y action para validar permiso
+            }
+            
+
+            ViewBag.isLogin = isLogin;
+            ViewBag.Logueado = Logueado;
+        }
+    }
+}
