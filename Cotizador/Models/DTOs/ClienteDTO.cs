106c988e35eca44224630564cfbded797a5774e6
César Humberto Cornejo Llanos <cesar.cornejo.l@hotmail.com>
2018-12-03 18:11:37 -0500

    se agregaron clases DTO

---
+++ Cotizador/Models/DTOs/ClienteDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cotizador.Models.DTOs
+{
+    public class ClienteDTO
+    {
+        public Guid idCliente { get; set; }
+               
+        public String codigo { get; set; }
+
+        public String razonSocialSunat { get; set; }
+
+        public String nombreComercial { get; set; }
+        public String tipoDocumentoIdentidadToString { get; set; }
+
+        public String ruc { get; set; }
+
+        public String ciudad_nombre { get; set; }
+
+        public String grupoCliente_nombre { get; set; }
+        public String responsableComercial_descripcion { get; set; }
+        public String supervisorComercial_descripcion { get; set; }
+        public String asistenteServicioCliente_descripcion { get; set; }
+
+        
+              public String tipoPagoFacturaToString { get; set; }
+
+        public String plazoCredito { get; set; }
+
+        public decimal creditoAprobado{ get; set; }
+        public bool bloqueado { get; set; }
+        
+
+
+
+
+
+
+    }
+    
+}
---
+++ Cotizador/Models/DTOs/NotaIngresoDTO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cotizador.Models.DTOs
+{
+    public class NotaIngresoDTO
+    {
+        public Guid idMovimientoAlmacen { get; set; }
+        public String serieNumeroNotaIngreso { get; set; }
+        public String pedido_numeroPedidoString  { get; set; }
+        public String motivoTrasladoString { get; set; }
+        public String usuario_nombre { get; set; }
+        public DateTime? fechaEmision { get; set; }
+        public DateTime? fechaTraslado { get; set; }
+        public String pedido_cliente_razonSocial { get; set; }
+        public String pedido_cliente_ruc { get; set; }
+        public String ciudadDestino_nombre { get; set; }
+        public String tipoExtornoToString { get; set; }
+        public String estadoDescripcion { get; set; }
+        public long numeroDocumento { get; set; }
+        public bool estaAnulado { get; set; }
+        public bool estaFacturado { get; set; }
+        public bool estaNoEntregado { get; set; }
+      //  public String serieNumeroGuia { get; set; }
+
+
+
+
+    }
+
+  
+                      
+                     
+                       
+}
