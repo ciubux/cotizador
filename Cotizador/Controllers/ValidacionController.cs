6eb1a126429eca3ad4edbd6ca92bc021896324e2
Yrving Ramirez <yrvingrl520@gmail.com>
2019-03-18 08:43:17 -0500

    alertas

---
+++ Cotizador/Controllers/ValidacionController.cs
@@ -0,0 +1,72 @@
+﻿using BusinessLayer;
+using Cotizador.Models;
+using Cotizador.ExcelExport;
+using Model;
+using Newtonsoft.Json;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cotizador.Controllers
+{
+    public class ValidacionController : ParentController
+    {
+
+        [HttpGet]
+        public ActionResult Pendientes()
+        {
+
+            this.Session[Constantes.VAR_SESSION_PAGINA] = (int)Constantes.paginas.AlertasValidacionPendientes;
+
+            AlertaValidacionBL bl = new AlertaValidacionBL();
+            Usuario usuario = (Usuario)this.Session[Constantes.VAR_SESSION_USUARIO];
+            List<AlertaValidacion> alertas = bl.getAlertasUsuario(usuario);
+            usuario.alertasList = alertas;
+
+            ViewBag.pagina = (int)Constantes.paginas.AlertasValidacionPendientes;
+            ViewBag.alertas = alertas;
+
+            return View();
+        }
+        
+        // GET: Origen
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Usuario usuario = (Usuario)this.Session[Constantes.VAR_SESSION_USUARIO];
+            if (!usuario.modificaOrigen)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View();
+
+        }
+
+        public String ValidarPendiente()
+        {
+            Guid idAlertaValidacion = Guid.Parse(Request["idAlertaValidacion"].ToString());
+            AlertaValidacionBL bl = new AlertaValidacionBL();
+
+            
+            Usuario usuario = (Usuario)this.Session[Constantes.VAR_SESSION_USUARIO];
+
+            bl.validarAlerta(idAlertaValidacion, usuario.idUsuario);
+
+            List<AlertaValidacion> alertas = bl.getAlertasUsuario(usuario);
+            usuario.alertasList = alertas;
+
+            this.Session[Constantes.VAR_SESSION_USUARIO] = usuario;
+            
+            return "{\"success\": 1}";
+        }
+    }
+
+
+}
---
+++ Model/AlertaValidacion.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Model
+{
+    public class AlertaValidacion
+    {
+        public Guid IdAlertaValidacion { get; set; }
+
+        public string nombreTabla { get; set; }
+
+        public string tipo { get; set; }
+        public string idRegistro { get; set; }
+
+        [Display(Name = "Estado:")]
+        public int Estado { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public Guid IdUsuarioCreacion { get; set; }
+        public DateTime FechaValidacion { get; set; }
+        public Guid IdUsuarioValidacion { get; set; }
+        public Usuario UsuarioCreacion { get; set; }
+        public Usuario usuario { get; set; }
+
+        public DataAlertaValidacion data { get; set; }
+
+
+        public string EstadoDesc => Estado == 1 ? "Validado" : "No Validado";
+
+        public bool EstadoCheck
+        {
+            get { return Estado == 1; }
+            set
+            {
+                Estado = value ? 1 : 0;
+            }
+        }
+
+        public string FechaCreacionDesc
+        {
+            get { return FechaCreacion.ToString("dd/MM/yyyy HH:mm:ss"); }
+        }
+
+        public string FechaCreacionFormatoFecha
+        {
+            get { return FechaCreacion.ToString(Constantes.formatoFecha); }
+        }
+
+        public string FechaValidacionDesc
+        {
+            get { return FechaValidacion.ToString("dd/MM/yyyy HH:mm:ss"); }
+        }
+
+        public string FechaValidacionFormatoFecha
+        {
+            get { return FechaValidacion.ToString(Constantes.formatoFecha); }
+        }
+
+        public string CambioDesc
+        {
+            get {
+                if (this.tipo == AlertaValidacion.CAMBIA_GRUPO_CLIENTE)
+                {
+                    return "Se cambio el Grupo del cliente \"" + this.data.ObjData + "\" de \"" + this.data.PrevData + "\" por \"" + this.data.PostData + "\" ";
+                }
+                return "";
+            }
+            
+        }
+
+        public static String CAMBIA_GRUPO_CLIENTE = "CAMBIA_GRUPO_CLIENTE";
+    }
+}
