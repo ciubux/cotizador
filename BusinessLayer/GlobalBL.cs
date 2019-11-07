4ed1355ebf1df4d3632534c2a80db15226028c02
Yrving Ramirez <yrvingrl520@gmail.com>
2019-11-07 10:27:00 -0500

    dev

---
+++ BusinessLayer/GlobalBL.cs
@@ -0,0 +1,20 @@
+﻿
+using DataLayer;
+using System.Collections.Generic;
+using System;
+using Model;
+using System.IO;
+
+namespace BusinessLayer
+{
+    public class GlobalBL
+    {
+        public void JobDiario()
+        {
+            using (var dal = new GlobalDAL())
+            {
+                dal.JobDiario();
+            }
+        }
+    }
+}
---
+++ DataLayer/GlobalDAL.cs
@@ -0,0 +1,27 @@
+﻿using Framework.DAL;
+using Framework.DAL.Settings.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Model;
+
+namespace DataLayer
+{
+    public class GlobalDAL : DaoBase
+    {
+        public GlobalDAL(IDalSettings settings) : base(settings)
+        {
+        }
+
+        public GlobalDAL() : this(new CotizadorSettings())
+        {
+        }
+
+
+        public void JobDiario()
+        {
+            var objCommand = GetSqlCommand("pu_jobDiario");
+            ExecuteNonQuery(objCommand);
+        }
+    }
+}
