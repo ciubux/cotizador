9d2f32848c179f59fac0717fc867684b638de3d6
Yrving Ramirez <yrvingrl520@gmail.com>
2019-12-03 12:44:04 -0500

    formula calculo precio neto excel cotizacion

--- Cotizador/ExcelExport/CotizacionDetalleExcel.cs
+++ Cotizador/ExcelExport/CotizacionDetalleExcel.cs
@@ -389,7 +389,9 @@ namespace Cotizador.ExcelExport
 
                     UtilesHelper.setValorCelda(sheet, i, "H", (double)det.precioLista, twoDecCellStyle);
                     UtilesHelper.setValorCelda(sheet, i, "I", (double)det.porcentajeDescuento, twoDecCellStyle);
-                    UtilesHelper.setValorCelda(sheet, i, "J", (double)det.precioNeto, twoDecCellStyle);
+                    UtilesHelper.setFormulaCelda(sheet, i, "J", "H" + i.ToString() + "*(1-(I" + i.ToString() + "/100))", twoDecCellStyle);
+
+                    //UtilesHelper.setValorCelda(sheet, i, "J", (double)det.precioNeto, twoDecCellStyle);
                     UtilesHelper.setValorCelda(sheet, i, "K", (double)det.flete, twoDecCellStyle);
                     UtilesHelper.setFormulaCelda(sheet, i, "L", "J" + i.ToString() + "+K" + i.ToString() + "", twoDecCellStyle);
                     UtilesHelper.setFormulaCelda(sheet, i, "M", "L" + i.ToString() + "*G" + i.ToString() + "", twoDecCellStyle);
