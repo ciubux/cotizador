9bc8c04e709d58713bca96175f98b4ec19866e48
Yrving Ramirez <yrvingrl520@gmail.com>
2021-11-29 01:32:36 -0500

    avances

---
+++ Cotizador/Models/DTOsShow/StockPedidoShow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cotizador.Models.DTOsShow
+{
+    public class StockPedidoShow
+    {
+        public String texto { get; set; }
+        public String textoStock { get; set; }
+        public int semaforoStock { get; set; }
+        public bool esStock { get; set; }
+    }
+    
+}
