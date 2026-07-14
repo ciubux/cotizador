a00614e6f6539145ba00902f7a3528c0f2290d5e
Yrving Ramirez <yrvingrl520@gmail.com>
2026-07-14 00:09:17 -0500

    avances

---
+++ Model/ClienteProductoReservadoSolicitud.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class ClienteProductoReservadoSolicitud : Auditoria
+    {
+        public Guid idClienteProductoReservadoSolicitud { get; set; }
+
+        public int estadoSolicitud { get; set; }
+
+        public int cantidadSolicitada { get; set; }
+
+        public int cantidadAprobada { get; set; }
+        public DateTime fechaAprobacion { get; set; }
+
+        public Usuario usuarioAprobacion { get; set; }
+
+        public ClienteProductoReservado clienteProductoReservado { get; set; }
+    }
+}
